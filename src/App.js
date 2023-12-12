ae4a3beaad179edb80e729bcebfab10cfec7e63a
azizhb03 <hbaziz03@gmail.com>
2023-12-12 05:01:56 +0100

    up2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,36 @@
 
 import './App.css';
 import { Navbar } from './component/Navbar/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ShopCategory from './pages/ShopCategory';
-import {Shop} from './pages/Shop';
+import { Shop } from './pages/Shop';
 import { LoginSignup } from './pages/LoginSignup';
-import {Product} from "./pages/Product"
+import { Product } from "./pages/Product"
 import { Commande } from './pages/commande';
+import { Footer } from './component/Footer/Footer';
+import men_banner from './component/assets/banner_mens.png'
+import women_banner from './component/assets/banner_women.png'
+import kid_banner from './component/assets/banner_kids.png'
 export default function App() {
   return (
     <div>
       <BrowserRouter>
-     <Navbar/>
-     <Routes>
-      <Route path='/' element={<Shop/>}/>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Shop />} />
 
-      <Route path='/mens' element={<ShopCategory category="mens"/>}/>
-      <Route path='/womens' element={<ShopCategory category="women"/>}/>
-      <Route path='/kids' element={<ShopCategory category="kid"/>}/>
-      <Route path='/product' element={<Product/>}/>
-        <Route path=':productId' element={<Product/>}/>
-      
-      
-     <Route path='/commande' element={<Commande/>}/>
-     <Route path='/login' element={<LoginSignup/>}/>
-     </Routes>
-     </BrowserRouter>
-     
+          <Route path='/Men' element={<ShopCategory banner={men_banner} category="men" />} />
+          <Route path='/Women' element={<ShopCategory banner={women_banner} category="women" />} />
+          <Route path='/kids' element={<ShopCategory banner={kid_banner} category="kid" />} />
+          <Route path='/product' element={<Product />} >
+          <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/commande' element={<Commande />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+
     </div>
   );
 };
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Hero from '../component/hero/Hero'
+import { Popular } from '../component/Popular/Popular'
+import { Offers } from '../component/Offers/Offers'
+import { NewCollections } from '../component/NewCollections/NewCollections'
+import { NewsLetter } from '../component/NewsLetter/NewsLetter'
 
 export const Shop = () => {
   return (
     <div>
       <Hero/>
+      <Popular/>
+      <Offers/>
+      <NewCollections/>
+      <NewsLetter/>
 
         
     </div>
